ed538c4241fcacda0bafb785a1cf0113bbee0d42
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-05-22 21:52:37 -0400

    add data user in page GitUser

--- lib/src/GitUser.dart
+++ lib/src/GitUser.dart
@@ -1,21 +1,32 @@
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:github_clone/getData/user.dart';
+import 'package:github_clone/src/models/abrirLink.dart';
 
 class GitUser extends StatefulWidget {
-  String username;
 
-  GitUser(this.username);
+  Map<String, dynamic> retorno;
+  GitUser(this.retorno);
 
   @override
   _GitUser createState() => _GitUser();
 }
 
 class _GitUser extends State<GitUser> {
-
   @override
   Widget build(BuildContext context) {
+    String login = widget.retorno['login'];
+    String avatar_url = widget.retorno['avatar_url'];
+    String html_url = widget.retorno['html_url'];
+    String followers_url = widget.retorno['followers_url'];
+    String following_url = widget.retorno['followers_url'];
+    String name = widget.retorno['name'];
+    String bio = widget.retorno['bio'];
+    String blog = widget.retorno['blog'];
+    int public_repos = widget.retorno['public_repos'];
+    int public_gists = widget.retorno['public_gists'];
+    int followers = widget.retorno['followers'];
+    int following = widget.retorno['following'];
+
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -33,19 +44,19 @@ class _GitUser extends State<GitUser> {
         ),
         backgroundColor: Colors.indigoAccent,
       ),
-      body: Container(
+      body: Container (
         child: Padding(
           padding: const EdgeInsets.fromLTRB(18, 25, 18, 0),
           child: Column(
             children: [
               Container(
-                width: 380,
-                height: 270,
+                width: 390,
+                height: 210,
                 child: Column(
                   children: [
                     Row(
                       children: [
-                        //container da imagem
+                        //  CONTAINER AVATAR_URL
                         Container(
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(100),
@@ -55,9 +66,7 @@ class _GitUser extends State<GitUser> {
                               image: DecorationImage(
                                   fit: BoxFit.fill,
                                   image: NetworkImage(
-                                    //colocar o link do perfil do usuario
-                                    //avatar url
-                                      'https://avatars.githubusercontent.com/u/48875867?v=4'
+                                      '${avatar_url}'
                                   )
                               )
                           ),
@@ -65,18 +74,22 @@ class _GitUser extends State<GitUser> {
                           height: 100,
                         ),
                         Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+                          padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
                           child: Container(
+                            width: 150,
+                            height: 100,
+                            // NOME E USERNAME (LOGIN)
                             child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
+                                // NOME
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
                                     Expanded(
                                       // Expanded utilizado para quebrar linha automatica ao extourar layout
                                       child: Text(
                                         //name
-                                        '${widget.username}',
+                                        '${name}',
                                         style: TextStyle(
                                           fontSize: 20,
                                           fontWeight: FontWeight.w900,
@@ -85,107 +98,126 @@ class _GitUser extends State<GitUser> {
                                     ),
                                   ],
                                 ),
+                                // LOGIN
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
                                     Text(
-                                      //login
-                                      "castelogui",
+                                      '${login}',
                                       style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black54
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black54
                                       ),
                                     ),
                                   ],
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Container(
-                                          child: Row(
-                                            children: [
-                                              Padding(
-                                                padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
-                                                child: Text(
-                                                  "20",
-                                                  style: TextStyle(
-                                                      fontSize: 15,
-                                                      color: Colors.black,
-                                                      fontWeight: FontWeight.bold
-                                                  ),
-                                                ),
-                                              ),
-                                              Text(
-                                                "followers",
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    color: Colors.black54
-                                                ),
-                                              ),
-                                            ],
-                                          )
-                                      ),
-                                      Container(
-                                          child: Row(
-                                            children: [
-                                              Padding(
-                                                padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
-                                                child: Text(
-                                                  "20",
-                                                  style: TextStyle(
-                                                      fontSize: 15,
-                                                      color: Colors.black,
-                                                      fontWeight: FontWeight.bold
-                                                  ),
-                                                ),
-                                              ),
-                                              Text(
-                                                "following",
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    color: Colors.black54
-                                                ),
-                                              ),
-                                            ],
-                                          )
-                                      ),
-                                    ],
-                                  ),
-                                )
                               ],
                             ),
-                            width: 180,
-                            height: 100,
                           ),
                         ),
-                        Container(
-                          width: 65,
-                          height: 40,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Follow",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white
+                        // BUTTON FOLLOW
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => abrirLink("${html_url}")));
+                          },
+                          child: Container(
+                            width: 65,
+                            height: 40,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Follow",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.indigoAccent,
-                              boxShadow: [
-                                BoxShadow(color: Colors.black, spreadRadius: 1)
-                              ]
+                              ],
+                            ),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.indigoAccent,
+                                boxShadow: [
+                                  BoxShadow(color: Colors.black, spreadRadius: 1)
+                                ]
+                            ),
                           ),
                         )
                       ],
+                    ),
+                    // FOLLOWERS E FOLLOWINGS
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          // FOLLOWERS
+                          Container(
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                  child: Text(
+                                    '${followers}',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "followers",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54
+                                  ),
+                                ),
+                              ],
+                            )
+                          ),
+                          // FOLLOWING
+                          Container(
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                  child: Text(
+                                    "${following}",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "following",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black54
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Text(
+                            "${blog}",
+                          ),
+                          Text(
+                            "${bio}"
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
