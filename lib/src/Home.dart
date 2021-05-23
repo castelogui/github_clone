70ef614bcc4e93306d0b7cef90c727afa24f81d8
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-05-22 21:51:44 -0400

    add method for get data api

--- lib/src/Home.dart
+++ lib/src/Home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:github_clone/GitUser.dart';
+import 'package:github_clone/src/GitUser.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -12,79 +14,101 @@ class _HomeState extends State<Home> {
   TextEditingController username = TextEditingController();
   String newUsername = '';
 
+  _obterRequesicao(String user)async{
+    var url = Uri.https('api.github.com', '/users/${user}');
+
+    http.Response resposta = await http.get(url);
+    if(resposta.statusCode == 200) {
+      Map<String, dynamic> retorno = json.decode(resposta.body);
+      setState(() {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => GitUser(retorno)));
+      });
+
+    }else{
+      print(resposta.body);
+      throw 'Erro, solicitação nao atendida';
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigoAccent,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('images/logo-buscagit.png', width: 280,),
-            Image.asset('images/logo-github.png', width: 180,),
-            Center(
-              child: Column(
-                children: [
-                  Text(
-                    'Username:',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      color: Colors.black
-                    ),
-                  ),
-                  Container(
-                    child: TextField(
-                      keyboardType: TextInputType.text,
-                      controller: username,
-                      decoration: InputDecoration(
-                        labelText: 'Usuario: '
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 90, 0, 0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('images/logo-buscagit.png', width: 280,),
+                Image.asset('images/logo-github.png', width: 180,),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Username:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.black
+                        ),
                       ),
-                    ),
-                    width: 225,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.white12,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () async{
-                      // ignore: unnecessary_statements
-                      setState(() {
-                        newUsername = username.text;
-                      });
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => GitUser(newUsername)));
-                      print(newUsername);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                      child: Container(
-                        width: 200,
-                        height: 40,
-                        child: Center(
-                          child: Text(
-                            'Buscar',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white
+                      Container(
+                        child: TextField(
+                          keyboardType: TextInputType.text,
+                          controller: username,
+                          decoration: InputDecoration(
+                            labelText: 'Usuario: '
+                          ),
+                        ),
+                        width: 225,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white12,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // ignore: unnecessary_statements
+                          setState(() {
+                            newUsername = username.text;
+                            print(newUsername);
+                          });
+                          _obterRequesicao(newUsername);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Container(
+                            width: 200,
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                'Buscar',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white
+                                ),
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.blue,
                             ),
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
